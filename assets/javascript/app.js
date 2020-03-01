a94d97d010bd0e066c398d160164ce26a0e41d3f
Quyen Le <heyquyenle@gmail.com>
2020-02-29 21:09:01 -0800

    updated the "still" & "animate"
    The gifs are still untill click on it then they will animate

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,7 +19,8 @@ $(document).ready(function(){
 
 $("button").on("click", function(){
     var person=$(this).attr("data-person");
-    var queryURL="https://api.giphy.com/v1/gifs/search?q=" + person +"&apikey=LNU9vZ1fEnoueTGgvpqZzKf0BLy1uK8Z";
+    //adding limit of 10 gifs
+    var queryURL="https://api.giphy.com/v1/gifs/search?q=" + person +"&apikey=LNU9vZ1fEnoueTGgvpqZzKf0BLy1uK8Z&limit=10";
     $.ajax({
         url: queryURL,
         method:"GET"
@@ -28,29 +29,37 @@ $("button").on("click", function(){
         var results = response.data;
         for (var i= 0; i < results.length; i++) {
             var gifDiv=$("<div>");
+            var gifView = results[i].images.fixed_height.url;
+            var still =results[i].images.fixed_height_still.url;
+            console.log(gifView);
             var rating= results[i].rating;
             var p= $("<p>").text("Rating: " + rating);
             var personImage= $("<img>");
-            personImage.attr("src", results[i].images.fixed_height.url);
-            personImage.attr("data-state", "still");
-            personImage.attr("data-position", i);
+            personImage.attr("src", still);
+            personImage.attr("data-animate", gifView);
+            personImage.attr("data-still", still);
+            personImage.attr("data-state", 'still');
             gifDiv.prepend(p);
             gifDiv.prepend(personImage);
             $("#gifs").prepend(gifDiv);
+    personImage.on('click', GifPlay);
         }
     });
-});
 
-$("#gif").on("click", function(){
+function GifPlay() {
     var state=$(this).attr("data-state");
-    var position=$(this).attr("data-position");
-    position= parseInt(position);
+    console.log(state);
+    // var position=$(this).attr("data-position");
+    // position= parseInt(position);
   if (state === "still") {
-        $(this).attr("src", results[position].images.fixed_height.url);
+        $(this).attr("src", $(this).data('animate'));
         $(this).attr("data-state","animate");
-      } else {
-        $(this).attr("src", results[position].images.fixed_height.url);
+      } 
+      else {
+        $(this).attr("src", $(this).data('still'));
         $(this).attr("data-state","still");
-      
-      }
-})
+      };
+      };
+
+   
+    });
